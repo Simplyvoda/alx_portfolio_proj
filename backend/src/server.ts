0f19af566d4cdb9f92e61f96f6977e8ad8747c21
Simplyvoda <efem.vodina@gmail.com>
2024-05-30 00:09:47 +0100

    changed cors options

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,16 @@ import routes from "./routes/index";
 import session from "express-session";
 import cookieParser from "cookie-parser";
 import { dbClient } from "../utils/db";
-// import cors from "cors";
+import cors from "cors";
+
+
+// Define CORS options
+const corsOptions = {
+  origin: '*', // Allow requests from this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow only specified methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow only specified headers
+};
+
 
 const app = express();
 
@@ -14,7 +23,7 @@ const port = process.env.PORT || 4001;
 app.use(bodyParser.json());
 // app.use(cookieParser());
 // Use CORS middleware
-// app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
